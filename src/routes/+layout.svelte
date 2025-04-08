4c5588032bf3dea856d0ac01042d59302aaa16e5
razz.ernesto@gmail.com <ernesto@muuvment.com>
2025-04-09 05:50:44 +0800

    feat: setup ui framework and tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { Toaster } from "$lib/components/ui/sonner";
+
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<Toaster />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Welcome to Meditrack</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { toast } from "svelte-sonner";
+</script>
+
+
+<div class="h-[100vh] flex items-center justify-center">
+  <div>
+    <h1 class="text-2xl font-bold">Welcome to Meditrack</h1>
+    <div class="flex gap-2">
+      <Button variant="default">Login</Button>
+      <Button variant="default" on:click={() => toast.success("Wecome to Meditrack")}>Click me</Button>
+    </div>
+  </div>
+</div>
+
